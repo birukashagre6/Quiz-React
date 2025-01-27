c3a1d6eae4b5ba2a376d743d44445a15a2c45380
birukashagre6 <mandave693@gmail.com>
2025-01-27 21:43:19 +0300

    adding the json-server logic for fake api and useReducer for managing state and other components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
-function App() {
-  return <div>recat quiz</div>;
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "RecievedData":
+      return {
+        ...state,
+        questions: action.data,
+        status: "ready",
+      };
+    case "FailedData":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unkown Error");
+  }
 }
 
-export default App;
+export default function App() {
+  const initialState = {
+    questions: [],
+    status: "loading",
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "RecievedData", payload: data }))
+      .catch((err) => dispatch({ type: "FailedData" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+}
